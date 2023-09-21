433ec77f29dff87480c5a7c576c4b9a27ef342a1
ranjankuldeep <ranjankuldeep19@gmail.com>
2023-09-21 16:51:53 +0530

    Logging and transport service implemented

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go_microservice
+
+go 1.21.0
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"context"
+	"log"
+)
+
 func main() {
 	println("Hello, World!")
+	svc := NewCatFactService("https://catfact.ninja/fact")
+	svc = NewLoggingService(svc)
+	apiServer := NewApiServer(svc)
+	log.Fatal(apiServer.Start(":8080"))
+
+	_, err := svc.GetCatFact(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
 }
